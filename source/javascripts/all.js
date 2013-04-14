53475b9f15f8592f2dce4748b5dd282a9f7586bd
Robert Syme <rob.syme@gmail.com>
2013-04-15 02:25:05 +0800

    Adding first layouts, styles and js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,3 @@
-//= require_tree .
+$(document).ready(function(){
+    $("#table-of-contents").appendTo("aside");
+});
---
+++ source/javascripts/post.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $("#table-of-contents").appendTo("aside");
+
+    
+    $(".outline-3").waypoint(function(direction) {
+        var a = $(this).attr('id');
+        var t = a.replace("outline-container", "#sec");
+        var s = 'a[href$="'.concat(t,'"]');
+        var n = $(s).parent().toggleClass("outline-3-scroll-selected", direction === 'up');
+    }, {
+        offset: function() {
+            return -$(this).height();
+        }
+    });
+
+    $(".outline-3").waypoint(function(direction) {
+        var a = $(this).attr('id');
+        var t = a.replace("outline-container", "#sec");
+        var s = 'a[href$="'.concat(t,'"]');
+        var n = $(s).parent().toggleClass("outline-3-scroll-selected", direction === 'down');
+    }, {
+        offset: '100%'
+    });
+
+
+    
+    $(".outline-2").waypoint(function(direction) {
+        var a = $(this).attr('id');
+        var t = a.replace("outline-container", "#sec");
+        var s = 'a[href$="'.concat(t,'"]');
+        var n = $(s).parent().toggleClass("outline-2-scroll-selected", direction === 'up');
+    }, {
+        offset: function() {
+            return -$(this).height();
+        }
+    });
+
+    $(".outline-2").waypoint(function(direction) {
+        var a = $(this).attr('id');
+        var t = a.replace("outline-container", "#sec");
+        var s = 'a[href$="'.concat(t,'"]');
+        var n = $(s).parent().toggleClass("outline-2-scroll-selected", direction === 'down');
+    }, {
+        offset: '100%'
+    });
+});
